a31831218047eb81c18d3448b55480e435191d3c
Jaejuna <anayana9988@gmail.com>
2023-03-22 15:24:34 +0900

    C ch02 done

---
+++ 이것이 c언어다/ch02/mainex.c
@@ -0,0 +1,6 @@
+int main(void)  // 머리
+{               // 몸체
+    10 + 20;
+
+    return 0;
+}
---
+++ 이것이 c언어다/ch02/noFormat.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d\n", 12);     //10 진수
+    printf("%d\n", 014);    //8 진수
+    printf("%d\n", 0xc);    //16 진수
+
+    return 0;
+}
