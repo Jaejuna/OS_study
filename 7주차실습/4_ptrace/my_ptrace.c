14a3cd5797a35159610853207fb6aa707cc93d5d
Jaejuna <anayana9988@gmail.com>
2023-04-18 02:25:59 +0900

    7주차 실습 끝

--- 7주차실습/4_ptrace/my_ptrace.c
+++ 7주차실습/4_ptrace/my_ptrace.c
@@ -7,8 +7,8 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define syscode_case(x)                                                        \
-  case x:                                                                      \
+#define syscode_case(x)                                                       
+  case x:                                                                     
     return #x
 
 const char *get_syscode(long);
@@ -19,6 +19,7 @@ int main() {
   pid_t child = fork();
   if (child == 0) {
     /* TODO: 자식 프로세스를 추적 가능하도록 설정 */
+    ptrace(PTRACE_TRACEME, 0, NULL, NULL);
     execl("/bin/ls", "ls", NULL);
   } else if (child < 0) {
     printf("Fork failed.\n");
@@ -26,10 +27,11 @@ int main() {
     int status;
     while (waitpid(child, &status, 0) && !WIFEXITED(status)) {
       /* TODO: 전체 레지스터 값을 가져오기 */
+      ptrace(PTRACE_GETREGS, child, NULL, &regs);
       fprintf(stderr, "[SYSCALL]:%-20s\t%5lld\n", get_syscode(regs.orig_rax),
               regs.orig_rax);
       /* TODO: 매 시스템 콜 마다 실행을 중단하게끔 설정하기 */
-    }
+          ptrace(PTRACE_SYSCALL, child, NULL, NULL);}
   }
   return 0;
 }
