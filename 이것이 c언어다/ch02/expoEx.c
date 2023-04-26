e2792e29b8c70fddf13301e0900ce392ed80068f
Jaejuna <anayana9988@gmail.com>
2023-04-26 18:17:30 +0900

    ch06 sample added

--- 이것이 c언어다/ch02/expoEx.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    printf("%.1lf\n", 1e6);
-    printf("%.7lf\n", 3.14e-5);
-
-    return 0;
-}
--- 이것이 c언어다/ch02/mainex.c
+++
@@ -1,6 +0,0 @@
-int main(void)  // 머리
-{               // 몸체
-    10 + 20;
-
-    return 0;
-}
